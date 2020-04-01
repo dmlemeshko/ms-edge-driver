a65cfa2262801fe10378b602bb32769cf148e90a
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2020-04-01 13:16:47 +0300

    fix HKey for Windows, add install script

--- src/browser.ts
+++ src/browser.ts
@@ -1,22 +1,22 @@
 import { exec } from 'child_process';
 import { promisify } from 'util';
 import * as _ from 'lodash';
-
 const execAsync = promisify(exec);
-const MAC_EDGE_BINARY_PATH = '/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge';
-const HKEY_WITH_EDGE_VERSION =
-  '\\SOFTWARE\\WOW6432Node\\Microsoft\\EdgeUpdate\\Clients\\{56EB18F8-B008-4CBD-B6D2-8C97FE7E9062}';
+const DEFAULT_EDGE_BINARY_PATH = '/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge';
+const DEFAULT_EDGE_HKEY =
+  '\SOFTWARE\\WOW6432Node\\Microsoft\\EdgeUpdate\\Clients\\{56EB18F8-B008-4CBD-B6D2-8C97FE7E9062}';
 
 const getBrowserBinaryOnWin = () => {
+  const edgeBinaryHKey = process.env.EDGE_HKEY || DEFAULT_EDGE_HKEY;
   let key;
   let subKey;
   try {
     const Key = require('windows-registry').Key;
     const windef = require('windows-registry').windef;
     key = new Key(windef.HKEY.HKEY_LOCAL_MACHINE, '', windef.KEY_ACCESS.KEY_READ);
-    subKey = key.openSubKey(HKEY_WITH_EDGE_VERSION, windef.KEY_ACCESS.KEY_READ);
-    const path = subKey.getValue('InstallLocation') as string;
-    const version = subKey.getValue('Version') as string;
+    subKey = key.openSubKey(edgeBinaryHKey, windef.KEY_ACCESS.KEY_READ);
+    const path = subKey.getValue('location') as string;
+    const version = subKey.getValue('pv') as string;
     return { path, version };
   } catch (err) {
     process.stdout.write(`MS Edge browser is not found in registry: ${err.stderr} \n`);
@@ -31,7 +31,7 @@ const getBrowserBinaryOnWin = () => {
 };
 
 const getBrowserBinaryOnMac = async (edgeBinaryPath: string | undefined) => {
-  const binaryPath = typeof edgeBinaryPath === 'undefined' ? MAC_EDGE_BINARY_PATH : edgeBinaryPath;
+  const binaryPath = typeof edgeBinaryPath === 'undefined' ? DEFAULT_EDGE_BINARY_PATH : edgeBinaryPath;
   try {
     const { stdout } = await execAsync(`"${binaryPath}" --version`);
     const found = stdout.toString().match(/\d{1,}.\d{1,}.\d{1,}.\d{1,}/g);

af17e36d6e3e511fbabe99a30c4dcf78e4d8b606
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2020-04-13 19:56:51 +0300

    rename variable

--- src/install.ts
+++ src/install.ts
@@ -2,10 +2,10 @@ import { installDriver } from '.';
 import * as Fs from 'fs';
 
 const edgePathFile = 'paths.json';
-const edgeDriverPath =
+const downloadOnInstall =
   process.env.npm_config_edgedriver_download_on_install || process.env.EDGEDRIVER_DOWNLOAD_ON_INSTALL;
 
-if (edgeDriverPath) {
+if (downloadOnInstall) {
   installDriver().then((paths) => {
     process.stdout.write(`MS Edge driver is set: ${JSON.stringify(paths)}\n`);
     Fs.writeFile(edgePathFile, JSON.stringify(paths), function (err: any) {
